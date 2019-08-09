c09d789f39409a3c18ed2aac0088c415a8124b68
debashish <debashish.saha@gmail.com>
2019-08-09 15:14:39 +0530

    first version

---
+++ currency-exchange-jpa-service/src/main/java/com/udemy/min/currencyexchangejpaservice/CurrencyExchangeJpaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.min.currencyexchangejpaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeJpaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeJpaServiceApplication.class, args);
+	}
+
+}
---
+++ currency-exchange-jpa-service/src/test/java/com/udemy/min/currencyexchangejpaservice/CurrencyExchangeJpaServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.udemy.min.currencyexchangejpaservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CurrencyExchangeJpaServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
